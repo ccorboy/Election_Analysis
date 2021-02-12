d3e421243794a8fe2bab23ea1a893634bce1bb49
ccorboy <conorcorboy@MacBook-Pro.local>
2021-02-12 12:55:13 -0800

    Adding the election audit code.

--- class_activites/f_string.py
+++
@@ -1,5 +0,0 @@
-#f-string practice
-
-my_votes = int(input("How many votes did you get in the election? "))
-total_votes= int(input("What is the total votes in the elections? "))
-print(f"I received {my_votes / total_votes * 100}% of the total votes. ")
